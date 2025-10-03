5c7dcfe19b3f90f5fc62c9dab7c374bea7d4992a
FilipeSodreTruth <filipe.truthcommerce@gmail.com>
2025-10-03 15:20:09 -0300

    deixando a seção 3d com altura maior

--- script.js
+++ script.js
@@ -449,7 +449,7 @@ new THREE.RGBELoader()
         if (!toggleContainer.querySelector('.toggle-hint')) {
             const hint = document.createElement('div');
             hint.className = 'toggle-hint';
-            hint.innerHTML = `<span>CLIQUE AQUI</span><svg class="hint-arrow" width="16" height="16" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M12 5v14M5 12l7 7 7-7" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/></svg>`;
+            hint.innerHTML = `<span>CLIQUE AQUI</span>`;
             toggleContainer.insertBefore(hint, toggleContainer.firstChild);
             
             // esconder o hint APENAS quando o toggle é usado (não quando clica no texto)
