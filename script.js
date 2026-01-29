27d7099f9c5136240994f2b4b6035d8bbece08cf
FilipeSodreTruth <filipe.truthcommerce@gmail.com>
2026-01-29 11:14:37 -0300

    alterando o inciio do fadeout pt 3

--- script.js
+++ script.js
@@ -981,7 +981,7 @@ new THREE.RGBELoader()
         ScrollTrigger.create({
             trigger: section,
             start: 'top 65%',   // ajusta quando começa a aparecer
-            end: 'bottom 50%',  // dispara o onLeave exatamente no final da seção
+            end: 'bottom 30%',  // dispara o onLeave mais cedo (quando o fim da seção atingir 30% da viewport)
             onEnter: self => {
                 ctas.forEach(c => c.classList.add('is-visible'));
                 // ensure 3D is visible when entering
